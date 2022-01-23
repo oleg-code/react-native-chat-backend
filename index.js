5d96f98b1f35c0c353778c352ca9ca1616050e81
oleg <cityavto0137@gmail.com>
2022-01-23 12:07:05 +0200

    add mongo

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require('socket.io')(server);
 const port = 3000;
+const mongoDB = "mongodb://localhost:27017/chat";
+const mongoose = require('mongoose');
+const Msg = require('./models/messages');
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('connected')
+}).catch(err => console.log(err))
 
 io.on("connection", socket => {
-  console.log("a user connected :D");
-  socket.on("chat message", msg => {
-    console.log(msg);
-    io.emit("chat message", msg);
+	Msg.find().then(result => {
+        socket.emit('output-messages', result)
+    })	
+	console.log("a user connected :D");
+
+	socket.on("chat message", msg => {
+		const message = new Msg({ msg });
+    	message.save().then(() => {  
+    		console.log(msg);
+    		io.emit("chat message", msg);
+	})
   });
 });
 
